f1b7249b9b27826e70ff8a2f52299937a8472067
HaiwangYu <yuhw.pku@gmail.com>
2020-04-10 12:09:07 -0400

    update

---
+++ hello.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace MyNamespace
+{
+class Foo
+{
+public:
+    Foo() {}
+
+protected:
+    int m_i;
+};
+} // namespace MyNamespace
+
+int main()
+{
+    std::cout << "Hello\n";
+    return 0;
+}
